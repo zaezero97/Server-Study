2fad1760077395f12f5aa5d56f8cea7adeb43b82
zaezero97 <xornjs1423@gmail.com>
2022-02-09 17:19:04 +0900

    :sparkles: TodoService 추가
    TodoService 추가

--- todo-server/src/main/java/org/example/repository/TodoRepository.java
+++ todo-server/src/main/java/org/example/repository/TodoRepository.java
@@ -4,5 +4,5 @@ import org.example.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-    
+
 }
